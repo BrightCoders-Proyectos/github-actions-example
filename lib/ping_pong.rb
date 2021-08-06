37fe018e73517a754c0b26cdd1c8dd9319836318
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2021-08-06 12:19:28 -0500

    Initial commit :ping_pong:

---
+++ lib/ping_pong.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ping_pong/version"
+
+module PingPong
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ping_pong_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe PingPong do
+  it "has a version number" do
+    expect(PingPong::VERSION).not_to be nil
+  end
+end
