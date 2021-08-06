cf22f1b79fccd0acc882eb9f06d686ae927de5d5
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2021-08-06 12:29:18 -0500

    Ping commit :fire:

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -2,7 +2,12 @@
 
 require_relative "ping_pong/version"
 
+# Ping: Only demo class
 module PingPong
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.ping
+    :pong
+  end
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe PingPong do
   it "has a version number" do
     expect(PingPong::VERSION).not_to be nil
   end
+
+  it "is alive" do
+    expect(PingPong.ping).to eq(:pong)
+  end
 end
